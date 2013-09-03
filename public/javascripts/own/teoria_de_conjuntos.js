ec62c53858b07986059f56bfaac48d64ef3cfeab
Daniel Arturo Rodriguez Rosas <darr.0725@gmail.com>
2013-09-03 00:06:03 -0500

    Interfaz terminada

--- public/javascripts/own/teoria_de_conjuntos.js
+++ public/javascripts/own/teoria_de_conjuntos.js
@@ -4,11 +4,9 @@ function start(){
                                 revert: true,
                                 revertDuration: 200});
 
-  $( ".droppable" ).droppable({
-    accept: ".draggable",
-    hoverClass: "droppable-hover",
-    drop: elementDropped
-  });
+  $( ".droppable" ).droppable({ accept: ".draggable",
+                                hoverClass: "droppable-hover",
+                                drop: elementDropped });
 }
 
 function elementDropped(event, ui){
@@ -17,23 +15,61 @@ function elementDropped(event, ui){
                             "data-element = '" + element + "'> "+
                               "<span class= 'element'>" + element + "</span>"+
                               "<span class='cross'> X </span>" +
-                            "</div>");
+                            "</div>"),
+      flag = false,
+      newElement,
+      temporalElements = $(this).children('.temporal-element'),
+      temporalElementsLength,
+      i;
 
-  $(this).append(elementContainer);
-  $('.cross').hide();
+  temporalElementsLength = temporalElements.length;
+  for (i = 1 ; i <= temporalElementsLength; i++) {
+    newElement = $('.temporal-element:nth-child(' + i +')').data('element');
 
-  $(".temporal-element").on('mouseenter', showCloseOption);
-  $(".temporal-element").on('mouseleave', hideCloseOption);
+    if(newElement == element){
+      flag = true;
+      break;
+    }
+  };
+
+  if(!flag){
+    $(this).append(elementContainer);
+    $('.cross').hide();
+
+    $(".temporal-element").on('mouseenter', showCloseOption);
+    $(".temporal-element").on('mouseleave', hideCloseOption);
+    $(".cross").on("click", removeElement);
+  }
 }
 
 function showCloseOption(){
   $(this).find(".element").hide();
-  $(this).find(".cross").fadeIn();
+  $(this).find(".cross").show();
 }
 
 function hideCloseOption(){
   $(this).find(".cross").hide();
-  $(this).find(".element").fadeIn();
+  $(this).find(".element").show();
+}
+
+function removeElement(){
+  $(this).parent().remove();
+}
+
+function union( subsetA, subsetB ){
+
+}
+
+function intersection( subsetA, subsetB ){
+
+}
+
+function complement(){
+
+}
+
+function operations( subsetA, subsetB ){
+
 }
 
 $(document).on('ready', start);
