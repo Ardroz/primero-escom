eca395e1c865379317b9d29d13fbb6a04fded14d
Daniel Arturo Rodriguez Rosas <darr.0725@gmail.com>
2013-09-08 14:37:37 -0500

    teoria de conjuntos terminado

--- public/javascripts/own/teoria_de_conjuntos.js
+++ public/javascripts/own/teoria_de_conjuntos.js
@@ -1,7 +1,5 @@
 function start(){
-  $( ".draggable" ).draggable({ snap: true, 
-                                snapMode: "outer",
-                                revert: true,
+  $( ".draggable" ).draggable({ revert: true,
                                 revertDuration: 200});
 
   $( ".droppable" ).droppable({ accept: ".draggable",
@@ -21,14 +19,11 @@ function elementDropped(event, ui){
       temporalElements = $(this).children('.temporal-element'),
       temporalElementsLength,
       i;
-
   temporalElementsLength = temporalElements.length;
   for (i = 1 ; i <= temporalElementsLength; i++) {
-    newElement = $('.temporal-element:nth-child(' + i +')').data('element');
-
-    if(newElement == element){
+    newElement = $(this).children('.temporal-element:nth-child(' + i +')').data('element');
+    if(newElement === element){
       flag = true;
-      break;
     }
   };
 
@@ -40,6 +35,8 @@ function elementDropped(event, ui){
     $(".temporal-element").on('mouseleave', hideCloseOption);
     $(".cross").on("click", removeElement);
   }
+
+  operations();
 }
 
 function showCloseOption(){
@@ -54,22 +51,139 @@ function hideCloseOption(){
 
 function removeElement(){
   $(this).parent().remove();
+  operations();
 }
 
-function union( subsetA, subsetB ){
+/*
+  Operaciones Teoria de Conjuntos
+*/
 
+function union( subsetA, subsetB ){
+  var subsetALength = subsetA.length,
+      subsetBLength = subsetB.length,
+      union = new Array(),
+      repeatedElements = new Array(),
+      repeatedElementsLength;
+
+  for (i = 0; i < subsetALength; i++) {
+    for (j = 0; j < subsetBLength; j++) {
+      if( subsetA[i] === subsetB[j]){
+        repeatedElements = repeatedElements.concat(subsetA[i]);
+      }
+    }
+  }
+
+  union = insertionSort( subsetA.concat( subsetB ) );
+
+  repeatedElementsLength = repeatedElements.length;
+  for (i = 0; i < repeatedElementsLength; i++) {
+    union.splice( union.indexOf(repeatedElements[i]), 1);
+  }
+
+  return union;
 }
 
 function intersection( subsetA, subsetB ){
+  var subsetALength = subsetA.length,
+      subsetBLength = subsetB.length,
+      intersection = new Array();
 
+  for (i = 0; i < subsetALength; i++) {
+    for (j = 0; j < subsetBLength; j++) {
+      if( subsetA[i] === subsetB[j]){
+        intersection = intersection.concat(subsetA[i]);
+      }
+    }
+  }
+
+  return intersection;
 }
 
-function complement(){
+function complement( universe, subset ){
 
+  var complement = new Array(),
+      i,
+      subsetLength = subset.length;
+
+  complement = insertionSort( universe.concat( subset ) );
+
+  for (i = 0; i < subsetLength; i++) {
+    complement.splice( complement.indexOf(subset[i]), 2);
+  }
+
+  return complement;
 }
 
-function operations( subsetA, subsetB ){
+function operations(){
+  var a = new Array(),
+      b = new Array(),
+      c = new Array(),
+      i,
+      subsetLength,
+      subsetA =  $('#subsetA').children('.temporal-element'),
+      subsetB =  $('#subsetB').children('.temporal-element'),
+      subsetC =  $('#subsetC').children('.temporal-element'),
+      universe = new Array();
 
+  for (i = 1; i <= 20; i++) {
+    universe[i - 1] = i;
+  }
+
+  a = insertionSort( jqueryToJsArray( subsetA, '#subsetA' ) );
+  b = insertionSort( jqueryToJsArray( subsetB, '#subsetB' ) );
+  c = insertionSort( jqueryToJsArray( subsetC, '#subsetC' ) );
+
+  /*
+    Las operaciones solicitadas
+    1) A^c U B^c
+    2) A^c N B
+    3) (A N B N C)^c
+  */
+
+  var operation1 = union( complement( universe, a ), complement( universe, b )),
+      operation2 = intersection( complement( universe, a ), b ),
+      operation3 = complement ( universe , intersection( a, intersection( b, c )));
+
+  $('.subsetA').text(a.join(', '));
+  $('.subsetB').text(b.join(', '));
+  $('.subsetC').text(c.join(', '));
+  $('.operation1').text(operation1.join(', '));
+  $('.operation2').text(operation2.join(', '));
+  $('.operation3').text(operation3.join(', '));
+}
+
+/*
+  Método de ordenación
+*/
+
+function insertionSort( array ) {
+  var size = array.length,
+      slot,
+      temporal;
+ 
+  for ( var item = 0; item < size; item++ ) { // outer loop     
+    temporal = array[item];
+    for ( slot = item - 1; slot >= 0 && array[slot] > temporal; slot-- ){ // inner loop
+      array[ slot + 1 ] = array[slot];
+    }
+    array[ slot + 1 ] = temporal;
+  }
+  return array;
+};
+
+/*
+  Convertir array de jquery a js
+*/
+
+function jqueryToJsArray( array , jqueryElement){
+  var arrayLength = array.length,
+      jsArray = new Array;
+
+  for (i = 0; i < arrayLength; i++) {
+    jsArray[i] = $(jqueryElement).children('.temporal-element:nth-child(' + (i+1) +')').data('element');
+  }
+
+  return jsArray;
 }
 
 $(document).on('ready', start);
