fba284808d806b6388d637e9326ea06a9a7d67f9
ardroz <darr.0725@gmail.com>
2013-11-05 13:50:51 -0600

    Maximo comun divisor finished

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -37,69 +36,3 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-function gcfEuclidsAlgorithm ( number1, number2 ) {
-  var a = new Array(),
-      b = new Array(),
-      d = new Array(),
-      k = new Array();
-
-  var i;
-
-  a[0] = 1;
-  b[0] = 0;
-  d[0] = number1;
-  k[0] = 0;
-
-  a[1] = 0;
-  b[1] = 1;
-  d[1] = number2;
-  k[1] = division( true, d[0], d[1]);
-
-  i = 2;
-
-  while ( true ){
-    a[i] = a[(i-2)] - ( a[(i-1)] * k[(i-1)]);
-    b[i] = b[i-2] - ( b[i-1] * k[i-1]);
-    d[i] = division( false, d[i-2], d[i-1] );
-    k[i] = division( true, d[i-1], d[i]);
-    if(d[i] <= 0){
-      break;
-    }
-    i++;
-  }
-
-  result = {
-    "x": a[i-1],
-    "y": b[i-1],
-    "gfc": d[i-1]
-  }
-
-  return result;
-}
-
-/*
-  The flag is to know if you want the quotient or the residue, 
-  return quotient if flag is true
-  return residue if flag is false
-*/
-function division ( flag, dividend, divider ) {
-  var quotient = 0,
-      residue = 1;
-
-  if ( divider > 0 ) {
-    while ( true ) {
-      residue = dividend - ( divider * quotient );
-
-      if( residue >= 0 ){
-        quotient++;
-      } else {
-        residue = dividend - ( divider * (quotient -1) );
-        quotient--;
-        break;
-      }
-    }
-  }
-
-  return flag ? quotient: residue;
-}
