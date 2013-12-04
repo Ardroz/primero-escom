f42d4fc9037fbd02ff74a5e25d7e7aed8b78145b
ardroz <darr.0725@gmail.com>
2013-12-03 21:36:36 -0600

    First part dijkstra

--- public/javascripts/own/dijkstra.js
+++ public/javascripts/own/dijkstra.js
@@ -15,6 +15,17 @@ function dijkstraController( scope ){
   };
 
   scope.connections = [];
+  
+  scope.addConnection = function () {
+    scope.connections.push(scope.temporal);
+
+    scope.list[scope.temporal.from].push(scope.temporal.to);
+    scope.temporal = {
+      from: 0,
+      to: 0,
+      weight: 0
+    };
+  };
 
   scope.setNodes = function () {
     var i = 0, len = scope.nodes;
@@ -22,20 +33,86 @@ function dijkstraController( scope ){
     for (i = 0; i < len; i++ ){
       scope.list.push([]);
     }
-
-    console.log(scope.list);
   };
 
-  scope.addConnection = function () {
-    scope.connections.push(scope.temporal);
-
-    scope.list[scope.temporal.from].push(scope.temporal.to);
-    console.log(scope.list);
-    scope.temporal = {
-      from: 0,
-      to: 0,
-      weight: 0
-    };
-
+  scope.dijkstra = function () {
+    dijkstra( scope );
   };
+}
+
+function dijkstra ( scope ) {
+  var i = 0,
+      nodesLength = scope.nodes,
+      indexes = [],
+      minor,
+      weights = [ 0 ];
+
+  for ( i = 1; i < nodesLength; i++) {
+    indexes.push( i );
+    if ( contains( scope.list[0], i ) ){
+      weights.push( searchWeight( scope.connections, 0, i ) );
+    } else {
+      weights.push( Infinity );
+    }
+  }
+
+  for ( i = 1; i < nodesLength; i++) {
+    minor = minInArray( temporalWeights ( indexes, weights ) );
+    console.log(minor);
+    indexes.splice( minor.index, 1);
+    console.log(indexes);
+  }
+}
+
+function contains ( array, obj ) {
+  var i = array.length;
+  while (i--) {
+    if (array[i] === obj) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function searchWeight ( array, from, to ) {
+  var weight;
+
+  array.forEach( function( element, index, array) {
+    if( element.to === to && element.from === from ){
+      weight = element.weight;
+    }
+  });
+
+  return weight;
+}
+
+function minInArray ( array ) {
+  var i = 0,
+      len = array.length,
+      minor = {
+        value: array[0],
+        index: 0
+      };
+
+  for ( i = 1; i < len; i++) {
+    if ( array[i] < minor.value ){
+      minor.value = array[i];
+      minor.index = i;
+    }
+  }
+
+  return minor;
+}
+
+function temporalWeights ( indexes, array ) {
+  var i = 0,
+      len = indexes.length,
+      weights = [];
+
+  for ( i = 0; i < len; i++ ) {
+    weights.push( array[indexes[i]] );
+  };
+  console.log(weights);
+  return weights;
 }
